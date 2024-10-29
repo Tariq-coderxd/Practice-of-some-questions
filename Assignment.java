4aedbb4a072da517a72708b31b9543a40a0023cd
Tariq-coderxd <Ariqxd01@gmail.com>
2024-10-29 22:13:55 +0530

    MY FIRST PRACTICE OF JAVA

---
+++ Assignment.java
@@ -0,0 +1,72 @@
+//package src.m;
+//
+//import java.util.Scanner;
+//
+//public class Assignment {
+//    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+
+        // ANSWER OF QUESTION NUMBER 1
+
+//        System.out.print("Please enter a number = ");
+//        int a = input.nextInt();
+//        if (a % 2 == 0) {
+//            System.out.print("The number is even");
+//        }else {
+//            System.out.print("The number is odd");
+//        }
+
+        // ANSWER OF QUESTION NUMBER 2
+
+//        System.out.print("Please give a name = ");
+//        String name = input.nextLine();
+//        System.out.print("Hello how are you doing " + name);
+
+        // ANSWER OF QUESTION NUMBER 3
+//        System.out.print("Please enter Principle value = ");
+//        int P = input.nextInt();
+//        System.out.print("please enter rate = ");
+//        float R = input.nextFloat();
+//        System.out.print("please enter Time = ");
+//        int T = input.nextInt(); // Time will be in year
+//        Float SI = P*R*T / 100 ;
+//        System.out.print("The value of simple interest is = " + SI );
+
+        // Answer of question number 4
+
+//        System.out.print("enter the first number = ");
+//        float num1 = input.nextFloat();
+//        System.out.print("Enter operator (+, -, *, /): ");
+//        char sign = input.next().charAt(0);
+//        System.out.print("enter the second number = ");
+//        float num2 = input.nextFloat();
+//        if (sign == '+'){
+//              System.out.println(num1 + num2);
+//         }else if (sign == '-'){
+//              System.out.println(num1 - num2);
+//         }else if (sign == '*'){
+//              System.out.println(num1 * num2);
+//         }else if (sign == '/'){
+//              System.out.println(num1 / num2);
+//        }
+
+        // ANSWER FOR QUESTION NUMBER 5
+//        System.out.print("Please enter amount in Rupees = ");
+//        double Rupees = input.nextDouble();
+//        double Dollar = Rupees * 0.011895556 ;
+//        System.out.print("convert amount in Dollars = " + Dollar);
+
+        //AUSWER FOR QUESTION NUMBER 6
+//        System.out.print("Please enter number1 : ");
+//        int number1 = input.nextInt();
+//        System.out.print("Please enter number2 : ");
+//        int number2 = input.nextInt();
+//        if (number1 == number2) {
+//            System.out.print("these numbers are equal");
+//        }else if (number1 > number2){
+//                System.out.print("number1 is greater than number2");
+//            }else
+//                System.out.print("number2 is greater than number1");
+ //   }
+//     }
+                                                                                                              THIS MY FIRST PRACTICE
